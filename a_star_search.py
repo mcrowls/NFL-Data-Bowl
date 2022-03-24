4a57945de4d5131f162f9f459f3b3d31d3c057c9
mcrowls <>
2022-03-24 10:00:00 +0000

    A* search algorithm

--- a_star_search.py
+++ a_star_search.py
@@ -21,24 +21,7 @@ def dist_between_points(point_1, point_2):
     return np.sqrt((point_1[0] - point_2[0])**2 + (point_1[1] - point_2[1]))
 
 
-# a* takes direction into account
-
-# need node weights for each step (g) and also a weight of each node to the end goal (h) (f = g+h)
-
-# distance of the path + how far they have to go from this
-
-# all nodes start with a distance of infinity
-
-# frame by frame, find the node to travel through with the longest defender arrival times
-# and the shortest distance to goal.
-
-# connect point with the lowest risk in one window to the next, find the optimal path to the end zone
-
-# What is the input? Returners Distance and Points.
-
-# Updates continuously in time
-
-
+'''
 returner_pos = [61.68, 29.08]
 
 csv = pd.read_csv('Frame1.csv').loc[:,['x', 'y', 't']]
@@ -48,33 +31,45 @@ ys = csv['y']
 ts = csv['t']
 
 returner_speed = 7
+'''
+
+# This function carries out A* search at each frame of defenders. There is still
+# a lot to consider e.g. whether the returner position inputted is the position
+# calculated from the last A* search or the returners actual position. There is
+# also no eventuality where there are no possible lines, and there is no eventuality
+# for when the returner has passed through all of the defenders.
 
 def a_star_search(lines, ts, returner_pos, returner_speed=7, goal_line=0, plotting=False):
+    # Initialise xs and ys
     xs = lines[:, 0]
     ys = lines[:, 1]
+    # Split up xs, ys and ts into groups of 20 (from the lines)
     arrays = []
     for array in [xs, ys, ts]:
         new_array = np.array_split(array, len(array)/20)
         arrays.append([list(arr) for arr in new_array])
     xs, ys, ts = arrays
     possible_points = []
+    # Find the point on each line with the highest expected arrival time
     for i in range(np.shape(xs)[0]):
         max_t_index = ts[i].index(np.max(ts[i]))
         longest_time_loc = [xs[i][max_t_index], ys[i][max_t_index]]
+        # Find the distance to goal from the point (shorter the better)
         scaled_dist_to_goal = distance_to_goal(longest_time_loc)/120
-        time_to_point = dist_between_points(returner_pos, longest_time_loc)/returner_speed/(120/7)
+        # Find the time to the point and only consider in the algorithm if the returner can get through
+        time_to_point = dist_between_points(returner_pos, longest_time_loc)/returner_speed/(120/returner_speed)
         if time_to_point < ts[i][max_t_index]:
             possible_points.append(Point(longest_time_loc, 2*time_to_point, scaled_dist_to_goal))
     scores = []
+    # Get the weighted scores (the weighting is a bit wrong)
     for point in possible_points:
-        print("loc:", point.xy, ", g:", point.g, ", h:", point.h)
-        scores.append(point.g + point.h)
+        scores.append(point.f)
 
     index_of_min = scores.index(np.min(scores))
-
+    # Find the point that the returner should aim for, and plot an arrow below if plotting==True
     point = possible_points[index_of_min].xy
     if plotting:
-        point_time = possible_points[index_of_min].g/2/(7/120)
+        point_time = possible_points[index_of_min].g/2/(returner_speed/120)
 
         plt.scatter(returner_pos[0], returner_pos[1], c='r')
         for i in range(np.shape(xs)[0]):
