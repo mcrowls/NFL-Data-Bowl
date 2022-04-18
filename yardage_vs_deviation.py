1270f31576be29dcd5099d1989003680a21844f8
Cassandra <cassandra.tablet55@gmail.com>
2022-04-18 17:31:04 +0100

    yardage vs deviation graph

--- yardage_vs_deviation.py
+++ yardage_vs_deviation.py
@@ -3,14 +3,41 @@ from visualisation_functions import process_play
 from helpers import input_folderpath
 import os
 import pandas as pd
+from multiprocessing import Pool
+import matplotlib.pyplot as plt
+import numpy as np
 
 results = pd.read_csv("results.csv")
-done_plays = results['play']
-print(done_plays)
 
-for play in os.listdir(input_folderpath+"/receiving_plays"):
-    #skip already done plays
+def processing(play):
     if play in results.values:
-        continue
+        return
     print(play)
     process_play(input_folderpath+"/receiving_plays/"+play,play)
+
+def create_graph():
+    df = pd.read_csv("results.csv")
+    df = df.dropna()
+    df = df.drop(df[df.yardage < 0].index)
+    
+    yardage = df['yardage']
+    deviation = df['deviation']
+    r = np.corrcoef(yardage, deviation)
+    print(r)
+    plt.scatter(deviation,yardage)
+    plt.show()
+
+def process_plays():
+    #Change this number depending on how many cores you want to use for this
+    pool = Pool(8)
+    pool.map(processing,os.listdir(input_folderpath+"/receiving_plays"))
+
+
+if __name__ == '__main__':
+
+    process_plays()
+    create_graph()
+
+
+
+        
